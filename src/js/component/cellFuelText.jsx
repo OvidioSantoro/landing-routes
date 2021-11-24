46f7b7e7d8ef73a7a027765e6bffe1bf105d8b72
AslanSN <alan.staub@hotmail.com>
2021-11-24 18:52:02 +0000

    First Commit

---
+++ src/js/component/cellFuelText.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const CellFuelText = () => {
+	return (
+		<>
+			<h2> Cell Fuel System </h2>
+			<span>
+				The fuel cell system transforms air and hydrogen into energy,
+				two elements that are found abundantly in nature. Beyond the
+				electricity generated to power the vehicle, the fuel cell emits
+				nothing but <b>water</b>. A High Technology Manufacturer,
+				Hextasium is the first automotive company to develop a fuel cell
+				offering exceptional performances in a high-end vehicle.
+			</span>
+			<p>
+				However, the system for efficiently and rapidly extracting
+				hydrogen from nature is highly polluting. For to date, 95% of
+				hydrogen is still produced by fossil fuels, because the cleanest
+				system, depending on the source of the electrivity, is
+				electrolysis, which is expensive and highly inefficient.{" "}
+			</p>
+		</>
+	);
+};
+
+export default CellFuelText;
---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+//import react into the bundle
+import React from "react";
+import ReactDOM from "react-dom";
+
+//include bootstrap npm library into the bundle
+import "bootstrap";
+
+//include your index.scss file into the bundle
+import "../styles/index.scss";
+
+//import your own components
+import Home from "./component/home.jsx";
+
+ReactDOM.render(<Hello>How are you?</Hello>, document.querySelector("#app"));
+//render your react application
+ReactDOM.render(<Home />, document.querySelector("#app"));
