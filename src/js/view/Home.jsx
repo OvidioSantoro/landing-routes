732d3d02abcb4bd2c31ec2a7d29ae4c2d43ec315
AslanSN <alan.staub@hotmail.com>
2022-01-05 19:32:31 +0000

    Nav&Tabs with pills deleted & implemented slide view

--- src/js/view/Home.jsx
+++ src/js/view/Home.jsx
@@ -3,31 +3,47 @@ import React from "react";
 // Components
 //Slides
 import Navbar from "./Slides/Navbar/Navbar.jsx";
-import NavTabs from "./Nav&Tabs/Nav&Tabs.jsx";
 import PhilosophySlide from "./Slides/Philosophy/Philosophy.jsx";
 import HydrogenSlide from "./Slides/Hydrogen/Hydrogen.jsx";
 import CellFuelSlide from "./Slides/CellFuel/CellFuel.jsx";
 import PlatConVisionSlide from "./Slides/PlataformConceptVision/PlataformConceptVision.jsx";
 import DesignOne from "./Slides/DesignPack/DesignOne/DesignOne.jsx";
 import DesignTwo from "./Slides/DesignPack/DesignTwo/DesignTwo.jsx";
-import Contact from "./Contact&Footer/Contact.jsx";
+import Contact from "./Slides/Contact&Footer/Contact.jsx";
 
-import NavTabsTest from "./NavTabsTest.jsx";
 
-// import Footer from "./Slides/Footer/Footer.jsx";
+import CarouselBtnIndicators from "./CarouselComponents/CarouselButtons/CarouselButtonsIndicators/CarouselButtonsIndicators.jsx";
+import CarControlButtons from "./CarouselComponents/CarouselButtons/CarouselControlButtons/CarouselControlButtons.jsx";
 
-// import CarouselBtnIndicators from "./CarouselComponents/CarouselButtons/CarouselButtonsIndicators/CarouselButtonsIndicators.jsx";
-// import CarControlButtons from "./CarouselComponents/CarouselButtons/CarouselControlButtons/CarouselControlButtons.jsx";
-
-// import Car from "./Slides/DesignPack/Car.png";
-// import Hextasium from "./Slides/Navbar/HextasiumLogo.png";
 
 import "./Home.scss";
 
 const Home = () => (
-	//TODO: NAVBAR
+	
 	<>
-		<NavTabsTest />
+		<div className="text-center mt-5 Slide aling-top">
+			<Navbar />
+			<div
+				id="carouselExampleCaptions"
+				className="carousel slide carousel-fade "
+				data-bs-ride="carousel"
+				data-bs-interval="false">
+				<CarouselBtnIndicators />
+
+				
+				<div className="carousel-inner">
+					<PhilosophySlide />
+					<HydrogenSlide />
+					<CellFuelSlide />
+					<PlatConVisionSlide />
+					<DesignOne />
+					<DesignTwo />
+					<Contact />
+				</div>
+				
+				<CarControlButtons />
+			</div>
+		</div>
 	</>
 );
 
